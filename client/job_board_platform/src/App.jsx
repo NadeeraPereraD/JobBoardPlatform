a16c25e6fd0533b389a2550b989f53cb8f7c250a
Nadeera Perera <nasa891128@gmail.com>
2024-11-09 08:42:39 +0530

    Created initial project

---
+++ client/job_board_platform/src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid2";
+import Navbar from "./layouts/Navbar";
+
+function App() {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    ...theme.applyStyles("dark", {
+      backgroundColor: "#1A2027",
+    }),
+  }));
+
+  return (
+    <div >
+      <Navbar/>
+      <Grid container spacing={2}>
+        <Grid size={8}>
+          <Item>size=8</Item>
+        </Grid>
+        <Grid size={4}>
+          <Item>size=4</Item>
+        </Grid>
+        <Grid size={4}>
+          <Item>size=4</Item>
+        </Grid>
+        <Grid size={8}>
+          <Item>size=8</Item>
+        </Grid>
+      </Grid>    
+    </div>
+  );
+}
+
+export default App;
